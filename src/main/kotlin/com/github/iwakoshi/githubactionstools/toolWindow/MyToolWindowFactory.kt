af1a465dfca1ef450d103f511d193da703eaa8fc
GitHub Action <action@github.com>
2025-04-25 22:09:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/iwakoshi/githubactionstools/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/iwakoshi/githubactionstools/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.iwakoshi.githubactionstools.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.iwakoshi.githubactionstools.MyBundle
+import com.github.iwakoshi.githubactionstools.services.MyProjectService
 import javax.swing.JButton
 
 
